97217adddbef06b8cd7168eceed6d5975e141e8c
Tim Glew <38923544+Toranagason@users.noreply.github.com>
2020-05-30 16:56:24 -0400

    rest of stored procedures

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -522,8 +522,8 @@ def tasks(list_id):
     db_connection = connect_to_database()  # connect to db
 
     # check if requested list belongs to the user
-    query = "SELECT `user_id` FROM lists WHERE `list_id` = '{}'".format(list_id)
-    cursor = execute_query(db_connection, query)
+    cursor = db_connection.cursor()
+    cursor.callproc('getListUserId', [list_id, ])
     rtn = cursor.fetchall()
     cursor.close()
     if rtn[0][0] != current_user.id:
@@ -534,8 +534,8 @@ def tasks(list_id):
 
     context = {}  # create context dictionary
 
-    query = "SELECT `name`, `description` FROM lists WHERE `list_id` = '{}'".format(list_id)  # get name/desc of list
-    cursor = execute_query(db_connection, query)
+    cursor = db_connection.cursor()
+    cursor.callproc('getTasks', [list_id, ])
     rtn = cursor.fetchall()  # run query
     cursor.close()
     context = {'list_name': rtn[0][0], 'list_desc': rtn[0][1], 'list_id': list_id}
@@ -573,9 +573,6 @@ def add_task():
     db_connection = connect_to_database()
     inputs = request.form.to_dict(flat=True)  # get form inputs from request
 
-    #query = """INSERT INTO `tasks` (`list_id`, `dataType_id`, `description`, `completed`)
-    #VALUES ('{}', '{}', \"{}\", '{}')".format(inputs['list_id'], inputs['task_type'], inputs['task_desc'], inputs['task_comp'])"""
-    #execute_query(db_connection, query).fetchall()  # execute query
     cursor = db_connection.cursor()
     cursor.callproc('addTask', [inputs['list_id'], inputs['task_type'], inputs['task_desc'], inputs['task_comp'], ])
     #Source for commit: https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-commit.html
@@ -592,8 +589,9 @@ def delete_task(task_id, list_id):
     Route to delete a task
     """
     db_connection = connect_to_database()
-    query = "DELETE FROM `tasks` WHERE `task_id` = '{}'".format(task_id)
-    cursor = execute_query(db_connection, query)
+    cursor = db_connection.cursor()
+    cursor.callproc('deleteTask', [task_id, ])
+    db_connection.commit()
     rtn = cursor.fetchall()
     cursor.close()
     db_connection.close() # close connection before returning
@@ -611,8 +609,8 @@ def update_task(list_id, task_id):
 
     # display current data
     if request.method == 'GET':
-        query = "SELECT * FROM `tasks` WHERE `task_id` ='{}'".format(task_id)  # get info of task
-        cursor = execute_query(db_connection, query)
+        cursor = db_connection.cursor()
+        cursor.callproc('getSingleTask', [task_id, ])
         rtn = cursor.fetchall()
         cursor.close()
         context = {'task_id': rtn[0][0], 'task_type': rtn[0][2], 'task_desc': rtn[0][3], 'task_comp': rtn[0][4], 'list_id': list_id}
@@ -626,10 +624,9 @@ def update_task(list_id, task_id):
         db_connection.close() # close connection before returning
         return render_template('update_task.html', context=context)
     elif request.method == 'POST':
-        query = "UPDATE `tasks` SET `dataType_id` = %s, `description` = %s, `completed` = %s WHERE `task_id` = %s"
-        data = (request.form['task_type'], request.form['task_desc'], request.form['task_comp'], task_id)
-        cursor = execute_query(db_connection, query, data)
-        rtn = cursor.fetchall()
+        cursor = db_connection.cursor()
+        cursor.callproc('updateTask', [request.form['task_type'], request.form['task_desc'], request.form['task_comp'], task_id, ])
+        db_connection.commit()
         cursor.close()
         db_connection.close()
         webapp.logger.info("Update tasks. User_id: %s, task_id: %s", current_user.id, task_id)
